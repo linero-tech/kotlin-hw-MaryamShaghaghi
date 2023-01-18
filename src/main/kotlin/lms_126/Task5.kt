b8c30ba45b8f8aca2e86365e19d1f949163cf56a
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-01-18 15:28:01 +0100

    solution task 5,6,7

--- src/main/kotlin/lms_126/Task5.kt
+++ src/main/kotlin/lms_126/Task5.kt
@@ -2,5 +2,23 @@ package lms_126
 
 fun task5(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        FOR number is 1 to 8
+        IF number is 1
+        OUTPUT "Monday"
+        IF number is 2
+        OUTPUT "Tuesday"
+        IF number is 3
+        OUTPUT "Wednesday"
+        IF number is 4
+        OUTPUT "Thursday"
+        IF number is 5
+        OUTPUT "Friday"
+        IF number is 6
+        OUTPUT "Saturday"
+        IF number is 7
+        OUTPUT "Sunday"
+        IF number is 8
+        OUTPUT "Choose number between 1 to 7"
+    """.trimIndent()
 }
--- src/main/kotlin/lms_126/Task6.kt
+++ src/main/kotlin/lms_126/Task6.kt
@@ -2,5 +2,9 @@ package lms_126
 
 fun task6(): String {
 
-    return TODO("Replace this 'TODO' with your solution")
+    return """
+        FOR number is 1 to 100
+        IF number % 5 = 0
+        OUTPUT number
+    """.trimIndent()
 }
