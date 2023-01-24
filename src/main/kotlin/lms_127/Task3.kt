0a2271b5753205f4a2fa19de5d649a67045fd8b3
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-01-24 17:09:18 +0100

    solution task 3 & 4

--- src/main/kotlin/lms_127/Task3.kt
+++ src/main/kotlin/lms_127/Task3.kt
@@ -1,13 +1,12 @@
 package lms_127
 
 fun task3(radius: Double): Double {
-
-    val radius = 2.0
     val pi = 3.1416
     val result = 2 * pi * radius;
     return result
 
 }
 fun main() {
-    task3(2.0);
+   val answer = task3(2.0);
+    println(answer)
 }
--- src/main/kotlin/lms_127/Task4.kt
+++ src/main/kotlin/lms_127/Task4.kt
@@ -1,13 +1,11 @@
 package lms_127
 
 fun task4(base: Double, height: Double): Double {
-
-    val base = 5.0
-    val height = 10.0
-    val result = 1/2 * base * height;
+    val result = 0.5 * base * height;
     return result
 
 }
 fun main() {
-    task4(5.0,10.0)
+   val answer = task4(5.0,10.0)
+    println(answer)
 }
