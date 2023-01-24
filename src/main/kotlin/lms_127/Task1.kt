ad200957ae7c3ca98bf80fb9ff40d798163b519c
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-01-24 10:59:14 +0100

    solutions for task 1 & 2

--- src/main/kotlin/lms_127/Task1.kt
+++ src/main/kotlin/lms_127/Task1.kt
@@ -2,5 +2,13 @@ package lms_127
 
 fun task1() {
 
-    TODO("Erase this line and write your solution")
+    val countryName = "Panama"
+    val capitalCity = "Panama city"
+    val currency = "USD"
+    println("Country: $countryName")
+    println("Capital: $capitalCity")
+    println("Currency: $currency")
+}
+fun main() {
+    task1()
 }
--- src/main/kotlin/lms_127/Task2.kt
+++ src/main/kotlin/lms_127/Task2.kt
@@ -2,5 +2,12 @@ package lms_127
 
 fun task2() {
 
-    TODO("Erase this line and write your solution")
+    val name = "Jane"
+    val country = "Sweden"
+    val month = "June"
+    println("Hello $name, you are from $country and you were born in $month")
+
+}
+fun main() {
+    task2()
 }
