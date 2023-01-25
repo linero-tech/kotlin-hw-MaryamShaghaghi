955ca13dd17d99e4aee76c4e8df948bffb2f0a40
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-01-25 09:36:49 +0100

    solution task 5

--- src/main/kotlin/lms_127/Task5.kt
+++ src/main/kotlin/lms_127/Task5.kt
@@ -5,8 +5,14 @@ fun task5(valueForA: Int, valueForB: Int): Pair<Int, Int> {
     var a = valueForA
     var b = valueForB
 
-    TODO("Your solutions goes in here.")
+    a = a - b
+    b = a + b
+    a = b - a
 
     // Do not erase or change this statement
     return Pair(a, b)
+}
+fun main() {
+    val answer = task5(1,2)
+    println(answer)
 }
