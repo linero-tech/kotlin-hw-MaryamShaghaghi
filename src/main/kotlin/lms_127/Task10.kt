4b46972ceff4d6f7707e488db601dd941f024e86
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-01-27 21:41:01 +0100

    solution task10

--- src/main/kotlin/lms_127/Task10.kt
+++ src/main/kotlin/lms_127/Task10.kt
@@ -1,21 +1,44 @@
 package lms_127
 
 fun task10_1(assessments: String): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = assessments.length
+    return result
 }
+/*fun main() {
+ val answer = task10_1("LMHHF")
+ println(answer)
+}
+*/
 
 fun task10_2(assessments: String): Char {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    val result = assessments[3]
+    return result
 }
+/* fun main() {
+    val answer = task10_2("LMFHMF")
+    println(answer)
+}
+*/
 
 fun task10_3(assessments: String): Char {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+val result = assessments.get(assessments.length/2)
+    return result
 }
 
-fun task10_4(assessments: String): String {
+/*fun main() {
+  val answer = task10_3("LMFHM")
+    println(answer)
+}
+*/
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+fun task10_4(assessments: String): String {
+    val result = assessments.substring(0,3)
+    return result
+}
+
+
+fun main() {
+
+    val answer = task10_4("LMFHM")
+    println(answer)
 }
