a561dff776fc3b63314cc0148c4c70bb6c50cc30
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-01-28 13:10:14 +0100

    final solution task 9

--- src/main/kotlin/lms_127/Task9.kt
+++ src/main/kotlin/lms_127/Task9.kt
@@ -1,10 +1,10 @@
 package lms_127
 
 fun task9(sentence: String, character: Char): Boolean {
-    val result = sentence.contains(character)
+    val result = sentence.contains(character, ignoreCase = true)
     return result
 }
 fun main() {
-    val answer = task9("I code In KOTLIN", 'I')
+    val answer = task9("I code In KOTLIN", 'i')
     println(answer)
 }
