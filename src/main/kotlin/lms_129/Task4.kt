3a1665caccc9006d6710dc05c3a171f01e9ffce5
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-02-03 21:28:31 +0100

    solution task 4

--- src/main/kotlin/lms_129/Task4.kt
+++ src/main/kotlin/lms_129/Task4.kt
@@ -1,6 +1,17 @@
 package lms_129
 
-fun task4(): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+fun task4(): Int
+{
+    var result = 0
+    for (i in 1..1000) {
+        if (i % 9 == 0) {
+            result += i
+        }
+    }
+    return result
+}
+fun main()
+{
+    val answer = task4()
+    println(answer)
 }
