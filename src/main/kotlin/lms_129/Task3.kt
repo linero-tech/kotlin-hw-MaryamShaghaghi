c90623a75c3f17b0ac05eff30672c9badb3a7539
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-02-03 21:03:21 +0100

    solution task 3

--- src/main/kotlin/lms_129/Task3.kt
+++ src/main/kotlin/lms_129/Task3.kt
@@ -1,6 +1,17 @@
 package lms_129
 
-fun task3(number: Int): Int {
+fun task3(number: Int): Int
+{
+    var result = 1
+          for (i in 1..number)
+    {
+        result *= i
+    }
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return result
+}
+fun main()
+{
+    val answer = task3(5)
+    println(answer)
 }
