826c1b6979565c99b29548d1ca6e10489e4c2191
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-02-06 11:21:07 +0100

    Update Task1.kt

--- src/main/kotlin/lms_129/Task1.kt
+++ src/main/kotlin/lms_129/Task1.kt
@@ -3,9 +3,16 @@ package lms_129
 fun task1(a: Int, b: Int): Int
 {
     var result = 0
-    for (i in a..b)
+    if (a >= b)
     {
-        result += i
+        return result
+    }
+    else
+    {
+        for (i in a..b)
+        {
+            result += i
+        }
     }
     return result
 }
