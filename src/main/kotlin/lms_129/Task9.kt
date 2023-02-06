bf4f4e99d11cc10ed4fc3fb0421dbca4d532e1d8
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-02-06 10:42:29 +0100

    solution task 9

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -1,6 +1,25 @@
 package lms_129
 
-fun task9(temperature: String): String {
+fun task9(temperature: String): String
+{
+    val tempString = temperature.dropLast(1)
+    val temp = tempString.toInt()
+    val unit = temperature.last()
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    return if (unit == 'f' || unit == 'F')
+    {
+        val otherUnit = (temp - 32) * 0.5556
+        val final = otherUnit.toInt()
+        "$final C"
+    } else {
+        val otherUnit = 1.8 * temp + 32
+        val final = otherUnit.toInt()
+        "$final F"
+    }
+
+}
+fun main ()
+{
+    val answer = task9("-22F")
+    println(answer)
 }
