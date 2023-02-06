a3c0d54c4592b06f5198ef5789d38d0682a83f4a
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-02-06 08:33:12 +0100

    solution task 8

--- src/main/kotlin/lms_129/Task8.kt
+++ src/main/kotlin/lms_129/Task8.kt
@@ -1,6 +1,17 @@
 package lms_129
 
 fun task8(number: Int): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var result = 0
+    var num = number
+    while (num > 0) {
+        val remainder = num % 10
+        result += remainder
+        num /= 10
+    }
+    return result
+}
+fun main()
+{
+  val answer = task8(123)
+  println(answer)
 }
