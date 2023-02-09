435f3e900a1a052420048b6386671f0aa3529e54
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-02-09 12:57:18 +0100

    Update Task9.kt

--- src/main/kotlin/lms_129/Task9.kt
+++ src/main/kotlin/lms_129/Task9.kt
@@ -2,23 +2,20 @@ package lms_129
 
 fun task9(temperature: String): String
 {
-    val tempString = temperature.dropLast(1)
-    val temp = tempString.toInt()
+    val temp = temperature.dropLast(1).toInt()
     val unit = temperature.last()
 
     return if (unit == 'f' || unit == 'F')
     {
         val convert = (temp - 32) * 0.5556
         val convert2 = convert.toInt()
-        val convert3 = "$convert2 C"
-        val result = convert3.replace("\\s".toRegex(),"")
+        val result = "$convert2 C".replace("\\s".toRegex(),"")
         result
 
     } else {
         val convert = 1.8 * temp + 32
         val convert2 = convert.toInt()
-        val convert3 = "$convert2 F"
-        val result = convert3.replace("\\s".toRegex(),"")
+        val result = "$convert2 F".replace("\\s".toRegex(),"")
         result
     }
 
