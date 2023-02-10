a72764e60ea7cfed8a231db4284c0611aa46ec47
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-02-10 09:56:22 +0100

    module 103 task10

--- src/main/kotlin/lms_129/Task10.kt
+++ src/main/kotlin/lms_129/Task10.kt
@@ -1,6 +1,42 @@
 package lms_129
 
-fun task10(password: String): Boolean {
+fun task10(password: String): Boolean
+{
+	if (password.length < 6 || password.length > 10)
+	{
+		return false
+	}
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+	var hasLowerCase = false
+	var hasUpperCase = false
+	var hasNumber = false
+	var hasSpecial = false
+
+
+	for (element in password)
+	{
+		if (element.isLowerCase())
+		{
+			hasLowerCase = true
+		}
+		else if (element.isUpperCase())
+		{
+			hasUpperCase = true
+		}
+		else if (element.isDigit())
+		{
+			hasNumber = true
+		}
+		else if (!element.isLetterOrDigit())
+		{
+			hasSpecial = true
+		}
+	}
+		return hasLowerCase && hasUpperCase && hasNumber && hasSpecial
 }
+
+fun main()
+	{
+		val answer = task10("&0hnhG")
+		println(answer)
+	}
