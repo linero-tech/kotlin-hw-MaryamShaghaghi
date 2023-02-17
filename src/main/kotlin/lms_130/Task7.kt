40a4b238954db10978c9692687d5191a5ee4126e
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-02-17 13:19:55 +0100

    solution task7 module 104

--- src/main/kotlin/lms_130/Task7.kt
+++ src/main/kotlin/lms_130/Task7.kt
@@ -1,6 +1,27 @@
 package lms_130
 
-fun task7(items: List<Int>): Int {
+fun task7(items: List<Int>): Int
+{
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+    var smallestValue = items.first()
+    return if (items.isEmpty())
+    {
+         0
+    }
+    else
+    {
+        items.forEach { number ->
+            if (number < smallestValue)
+            {
+                smallestValue = number
+            }
+        }
+        return  smallestValue
+    }
+
+}
+fun main()
+{
+    val result = task7(listOf(10, 5, 11))
+    println(result)
 }
