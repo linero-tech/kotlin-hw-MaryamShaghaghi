9b2d97c7bbb775f8b863639cd2a4ac955945eda4
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-02-17 10:28:32 +0100

    solution task2 module 104

--- src/main/kotlin/lms_130/Task2.kt
+++ src/main/kotlin/lms_130/Task2.kt
@@ -2,5 +2,28 @@ package lms_130
 
 fun task2(items: List<Int>): Int {
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+
+
+    var result = 0
+    return if (items.isEmpty())
+    {
+        0
+    }
+    else
+    {
+
+        items.forEachIndexed { index, number ->
+            if (index % 2 == 0)
+            {
+                result += number
+            }
+        }
+        return result
+    }
 }
+fun main()
+{
+    val result = task2(listOf(1, 2, 3, 4))
+    println(result)
+}
+
