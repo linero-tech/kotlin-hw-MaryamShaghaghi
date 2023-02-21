2eb1f079f3576df20897574d35dc1fae8e3d2bf9
MaryamShaghaghi <122574719+MaryamShaghaghi@users.noreply.github.com>
2023-02-21 10:42:29 +0100

    solution task11 module 104

--- src/main/kotlin/lms_130/Task11.kt
+++ src/main/kotlin/lms_130/Task11.kt
@@ -1,28 +1,43 @@
 package lms_130
 
-fun task11_1(guests: List<String>): Int {
-
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+fun task11_1(guests: List<String>): Int
+{
+	var totalGuests = 0
+	guests.forEach { element ->
+		totalGuests += 1
+	}
+	return totalGuests
 }
 
-fun task11_2(guests: List<String>, condition: String): List<String> {
+fun task11_2(guests: List<String>, condition: String) : List<String>
+{
+	val filterGuests = mutableListOf<String>()
+	guests.forEach { element ->
+		val gustsCondition = "-${element.last()}"
 
-    return TODO("Replace this 'TODO' with the variable 'result'. Do not erase the 'return' keyword")
+		if (gustsCondition == condition)
+		{
+			filterGuests.add(element)
+		}
+	}
+      return filterGuests.toList()
+
 }
 
-fun main() {
+fun main()
+{
 
-    // Change the condition to either "-V", "-A", or "-K" to test your code under different conditions
-    val conditionOfInterest = "-V"
-    val listOfAttendees = listOf(
-        "Stéphanie-A",
-        "Edmée-K",
-        "Maëlla-K",
-        "Océanne-K",
-        "Géraldine-K",
-        "Maëline-K",
-    )
+	// Change the condition to either "-V", "-A", or "-K" to test your code under different conditions
+	val conditionOfInterest = "-K"
+	val listOfAttendees = listOf(
+		"Stéphanie-A",
+		"Edmée-K",
+		"Maëlla-K",
+		"Océanne-K",
+		"Géraldine-K",
+		"Maëline-K",
+	)
 
-    println("The event has a total of ${task11_1(listOfAttendees)} attendees.")
-    println("The attendees with condition $conditionOfInterest are ${task11_2(listOfAttendees, conditionOfInterest)}")
+	println("The event has a total of ${task11_1(listOfAttendees)} attendees.")
+	println("The attendees with condition $conditionOfInterest are ${task11_2(listOfAttendees, conditionOfInterest)}")
 }
